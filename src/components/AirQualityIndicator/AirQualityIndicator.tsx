abccf61209840081d305623426ff754c33f71791
Jinnan Zhang <vannizhang@gmail.com>
2020-09-17 07:31:18 -0700

    add description table

--- src/components/AirQualityIndicator/AirQualityIndicator.tsx
+++ src/components/AirQualityIndicator/AirQualityIndicator.tsx
@@ -20,6 +20,44 @@ const ColorLookup:Record<AirQualityCategory, string> = {
     'Hazardous': '#942789',
 }
 
+const DescriptionLookup: {
+    category: AirQualityCategory
+    amount: string;
+    description: string;
+}[] = [
+    {
+        category: 'Good',
+        amount: '0 - 50',
+        description: 'Air pollution poses little or no risk.'
+    },
+    {
+        category: 'Moderate',
+        amount: '51 - 100',
+        description: 'may be a risk for people who are unusually sensitive to air pollution.'
+    },
+    {
+        category: 'Unhealthy for Sensitive Groups',
+        amount: '101 - 150',
+        description: 'The general public is less likely to be affected'
+    },
+    {
+        category: 'Unhealthy',
+        amount: '151 - 200',
+        description: 'Some members of the general public may experience health effects'
+    },
+    {
+        category: 'Very Unhealthy',
+        amount: '201 - 300',
+        description: 'The risk of health effects is increased for everyone.'
+    },
+    {
+        category: 'Hazardous',
+        amount: '301+',
+        description: 'Emergency conditions: everyone is more likely to be affected'
+    }
+]
+
+
 const AirQualityIndicator:React.FC<Props> = ({
     data
 }:Props) => {
@@ -30,8 +68,8 @@ const AirQualityIndicator:React.FC<Props> = ({
 
         return keys.map(key=>{
 
-            const catgeory:AirQualityCategory = data[key];
-            const color = ColorLookup[catgeory];
+            const categeory:AirQualityCategory = data[key];
+            const color = ColorLookup[categeory];
             const forecastTime = key === 'current' 
                 ? 'Current' 
                 : stringFns.capitalizeFirstLetter(key) + `'s forecast`
@@ -76,7 +114,7 @@ const AirQualityIndicator:React.FC<Props> = ({
                         'lineHeight': '1.25rem',
                         'fontSize': '14px'
                     }}>
-                        <span>{catgeory}</span>
+                        <span>{categeory}</span>
                     </div>
                 </div>
 
@@ -85,6 +123,61 @@ const AirQualityIndicator:React.FC<Props> = ({
         })
 
     }
+
+    const getTableForDescriptions = ()=>{
+
+        // active AirQualityCategory that's are dispalyed
+        const activeCategories: AirQualityCategory[]  = []
+
+        const keys = Object.keys(data);
+
+        keys.forEach(key=>{
+            const catgeory:AirQualityCategory = data[key];
+
+            if(activeCategories.indexOf(catgeory) === -1){
+                activeCategories.push(catgeory);
+            }
+
+        });
+
+        const rows = DescriptionLookup
+            .filter(d=>{
+                return activeCategories.indexOf(d.category) > -1
+            })
+            .map(d=>{
+                const { category, amount, description } = d;
+
+                return (
+                    <tr 
+                        key={category}
+                        className='font-size--3'
+                        style={{
+                            'background': ColorLookup[category],
+                            'textShadow': '0 0 3px #000',
+                            'color': '#fff'
+                        }}
+                    >
+                        <td>
+                            <span>{category}</span>
+                            <br/>
+                            <span>{amount}</span>
+                        </td>
+                        <td>
+                            {description}
+                        </td>
+                    </tr>
+                )
+            });
+        
+        return(
+            <table className='table table-plain'>
+                <tbody>
+                    { rows }
+                </tbody>
+            </table>
+        );
+
+    };
 
     return data ? (
         <div className="trailer-half">
@@ -95,11 +188,16 @@ const AirQualityIndicator:React.FC<Props> = ({
             <div
                 style={{
                     'display': 'flex',
-                    'justifyContent': 'center'
+                    'justifyContent': 'center',
+                    'marginBottom': '.75rem'
                 }}
             >
                 { getIndicators() }
             </div>
+
+            {
+                getTableForDescriptions()
+            }
         </div>
 
     ) : null;
